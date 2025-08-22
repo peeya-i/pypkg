3bba88dcf4a5d9a0a6863a3b2d2c9bb1133f2201
peeya-i <pi3860@gmail.com>
2025-08-22 14:55:27 -0700

    Added check for package already  imported

--- pypkg/__init__.py
+++ pypkg/__init__.py
@@ -7,20 +7,37 @@ def show():
     print("pylib-T", __version__)
 
 def testImport(library:str):
-try:
-    # Using check=False so that a non-zero exit code does not raise a CalledProcessError
-    returnmsg = subprocess.run(['pip', 'show', f'{library}'], capture_output=True, text=True, check=False)
-    if (returnmsg.returncode != 0):
-        print(f"Library {library} is not installed. Trying to install it...")
-        subprocess.run(['pip', 'install', f'{library}'], capture_output=True, text=True, check=True)
-    imported_module = importlib.import_module(library)
-    print(f"Successfully imported module: {library}") # Added a success message
+    if library in sys.modules:
+        print(f"Library {library} is already imported.")
+        return True
 
-except ImportError:
-    print(f"Library {library} is NOT installed and could not be imported.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+    try:
+        # Using check=False so that a non-zero exit code does not raise a CalledProcessError
+        returnmsg = subprocess.run(['pip', 'show', f'{library}'], capture_output=True, text=True, check=False)
+        if (returnmsg.returncode != 0):
+            print(f"Library {library} is not installed. Trying to install it...")
+            installmsg = subprocess.run(['pip', 'install', f'{library}'], capture_output=True, text=True, check=False)
+            if installmsg.returncode != 0:
+                print(f"Failed to install library {library}.")
+                return False # Return False if installation fails
+        else:
+            print(f"Library {library} is already installed.")
 
+        try:
+            imported_module = importlib.import_module(library)
+        except:
+            print(f"Failed to import library {library}.")
+            return False # Return False if import fails
+
+        print(f"Successfully imported module: {library}") # Added a success message
+        return True # Return True if import is successful
+
+    except ImportError:
+        print(f"Library {library} is NOT installed and could not be imported.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    return False # Return False for any other exception
 
 if __name__ == '__main__':
     show()
